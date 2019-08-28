33be3f370dca4a32561fd65915455645ab3f08ad
vinod <knaidu.vinod@gmail.com>
2019-08-28 09:25:45 +0530

    restructure and moving to typescript

--- servervisor-client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import App from './App.js'
-
-ReactDOM.render(
-  <App text="Hello...!<br>My first MERN stack application...!!!!!"/>,
-  document.getElementById('root')
-)
